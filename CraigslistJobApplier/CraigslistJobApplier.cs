6535b1bb646815d7713826403e44fd1cf0fb8192
Jordan Shackelford <jordanshackelford2@gmail.com>
2015-01-25 00:28:30 -0600

    initial publication

---
+++ CraigslistJobApplier/CraigslistJobApplier.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace CraigslistJobApplier
+{
+    class CraigslistJobProducer
+    {
+        public List<String> ExtractJobUrls()
+        {
+            var url = "http://seattle.craigslist.org/search/sof?";
+            var html = GetHtml(url);
+
+            var regex = new Regex(@"\/...\/sof\/\d+?.html"); // \/...\/sof\/\d+?.html
+
+            var tailUrls = regex.Matches(html)
+                .Cast<Match>()
+                .Select(x => x.Value)
+                .ToList();
+
+            //add root url
+            var rootUrl = url.Replace("/search/sof", String.Empty);
+
+            var jobUrls = new List<String>();
+
+            foreach (var tailUrl in tailUrls)
+                jobUrls.Add(rootUrl + tailUrl);
+
+            return jobUrls.Distinct().ToList();
+        }
+
+        private String GetHtml(String url)
+        {
+            using(var client = new WebClient())
+            {
+                Stream data = client.OpenRead(url);
+                StreamReader reader = new StreamReader(data);
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
---
+++ CraigslistJobApplier/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CraigslistJobApplier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var craigslistApplier = new CraigslistJobProducer();
+            var jobUrls = craigslistApplier.ExtractJobUrls();
+
+            foreach(var jobUrl in jobUrls)
+                System.Diagnostics.Process.Start(jobUrl);
+        }
+    }
+}
