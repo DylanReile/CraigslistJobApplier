d83dccb64c3d98c2bff82013751d5d9f58749565
DylanReile <dylanbreile@gmail.com>
2015-10-19 01:20:24 -0400

    added test cases that filter out all results

--- CraigslistJobApplierTests/JobFiltererTests.cs
+++ CraigslistJobApplierTests/JobFiltererTests.cs
@@ -13,6 +13,7 @@ namespace CraigslistJobApplierTests
         [TestCase(new String[] { "manager" }, 2, TestName = "BlackListedTitleWords_One")]
         [TestCase(new String[] {"manager", "developer"}, 1, TestName = "BlackListedTitleWords_Two")]
         [TestCase(new String[] { "MaNaGeR" }, 2, TestName = "BlackListedTitleWords_CaseInsensitivity")]
+        [TestCase(new String[] { "Rockstar" }, 0, TestName = "BlackListedTitleWords_FilterOutAll")]
         public void BlackListedTitleWords(String[] blacklistedTitleWords, int remainingJobCount)
         {
             //arrange
@@ -29,6 +30,7 @@ namespace CraigslistJobApplierTests
         [TestCase(new String[] { "designer", ".NET" }, 1, TestName = "BlackListedDescriptionWords_Two")]
         [TestCase(new String[] { "data" }, 2, TestName = "BlackListedDescriptionWords_One")]
         [TestCase(new String[] { "dAtA" }, 2, TestName = "BlackListedDescriptionWords_CaseInsensitivity")]
+        [TestCase(new String[] { "Jedi" }, 0, TestName = "BlackListedDescriptionWords_FilterOutAll")]
         public void BlackListedDescriptionWords(String[] blacklistedDescriptionWords, int remainingJobsCount)
         {
             //arrange
@@ -45,6 +47,7 @@ namespace CraigslistJobApplierTests
         [TestCase(new String[] { "Senior" }, 2, TestName = "WhitelistedTitleWords_One")]
         [TestCase(new String[] { "Senior", "ETL" }, 2, TestName = "WhitelistedTitleWords_Two")]
         [TestCase(new String[] { "SEniOR" }, 2, TestName = "WhitelistedTitleWords_CaseInsensitivity")]
+        [TestCase(new String[] { "Billion" }, 0, TestName = "WhitelistedTitleWords_FilterOutAll")]
         public void WhitelistedTitleWords(String[] whitelistedTitleWords, int remainingJobsCount)
         {
             //arrange
@@ -61,6 +64,7 @@ namespace CraigslistJobApplierTests
         [TestCase(new String[] { "C#" }, 2, TestName = "WhitelistedDescriptionWords_One")]
         [TestCase(new String[] { ".NET", "designer" }, 2, TestName = "WhitelistedDescriptionWords_Two")]
         [TestCase(new String[] { ".nEt" }, 1, TestName = "WhitelistedDescriptionWords_CaseInsensitivity")]
+        [TestCase(new String[] { "Billion" }, 0, TestName = "WhitelistedDescriptionWords_FilterOutAll")]
         public void WhitelistedDescriptionWords(String[] whitelistedDescriptionWords, int remainingJobsCount)
         {
             //arrange
@@ -78,9 +82,9 @@ namespace CraigslistJobApplierTests
         {
             return new List<Job>()
             {
-                new Job() { Title= "Senior Developer", Description = "C# and .NET lead", EmailAddress = "senior@dev.com" },
-                new Job() { Title= "UX manager", Description = "User interfaces and experience. Designer", EmailAddress ="ux@manager.com" },
-                new Job() { Title = "Senior ETL Engineer", Description="C# , ETL data migrations and data modeling", EmailAddress = "etl@engineer.com" }
+                new Job() { Title= "Senior Developer Rockstar", Description = "C# and .NET lead. Jedi", EmailAddress = "senior@dev.com" },
+                new Job() { Title= "UX manager Rockstar", Description = "User interfaces and experience. Designer -- Jedi", EmailAddress ="ux@manager.com" },
+                new Job() { Title = "Senior ETL Engineer Rockstar", Description="C# , ETL data migrations and data modeling -- Jedi", EmailAddress = "etl@engineer.com" }
             };
         }
     }
